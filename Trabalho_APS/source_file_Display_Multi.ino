026a60ecd366c69fa7b1621895b943a65eff838e
Vinícius Abadia <viniciusabadiapc@gmail.com>
2020-05-03 03:09:52 -0300

    Incrementando ao LeTeclado
    Criando condições adicionais aos botões em respeito ao funcionamento do projeto visando maior desempenho e menor uso do processador

--- Trabalho_APS/source_file_Display_Multi.ino
+++ Trabalho_APS/source_file_Display_Multi.ino
@@ -10,8 +10,8 @@
 
 
 #define PD7_C_TEC  (1<<7)       //PORTD7, em 0 seleciona os Display, em 1 seleciona o Teclado  
-#define PD6_DIS_COL_A  (1<<6)   //PORTD6,5 respectivamente A,B Variam juntos de 00,01,10,11 e Varrem os Displays e o Teclado
-#define PD5_DIS_COL_B  (1<<5)
+#define A_DIS_COL  (1<<6)       //PORTD6,5 respectivamente A,B Variam juntos de 00,01,10,11 e Varrem os Displays e o Teclado
+#define B_DIS_COL  (1<<5)       //Sendo A o mais significativo.
 #define LE  (1<<4)              //PORTD4 em '0' habilita o 4511 permitindo a alteraçao dos displays, em '1' trava as entradas do mesmo, segue a mesma logica do PORTD7
 
 //=== Variaveis Globais =============================================================     
@@ -23,16 +23,16 @@ boolean Flag_botao1 = 0x00;      //Flags de validação de aperto de botões, pa
 boolean Flag_botao2 = 0x00;
 boolean Flag_botao3 = 0x00;
 boolean Flag_botao4 = 0x00;
-boolean Flag_Stop = 0x00;
+boolean Flag_Start = 0x00;
 
 int aux = 40;                   //Tempo de delay da multiplexação dos Displays.
 int aux2 = 0;                   //Auxiliar de contagem.
 
-int indice[4][4] ={ 1,2,3,10,   //contem o respectivo valor da tecla.
+/*int indice[4][4] ={ 1,2,3,10,   //contem o respectivo valor da tecla.
                     4,5,6,11,
                     7,8,9,12,
                     14,0,15,13};
-
+*/
 //=== Funções Auxiliares ===    Declaração de Funções.
 void Multiplexacao();
 void LeTeclado();
@@ -47,7 +47,7 @@ void setup() {
 //=== Loop Infinito ===============================================================
 void loop() {                   
   Multiplexacao();              //Chama a Função, que coloca os respectivos numero nos displays e efetua a MULTIPLEXAÇÃO.
-  Counter();                    //Chama a Função, efetua a contagem de numeros e os coloca nos respectivas variaveis.
+  Numero_Display();                    //Chama a Função, efetua a contagem de numeros e os coloca nos respectivas variaveis.
 }
 //=== Funções Auxiliares ==========================================================
 void Multiplexacao(){
@@ -74,10 +74,13 @@ void Multiplexacao(){
 }
 //================================================================================
 void Numero_Display (int numero){
+  /*
     contador_N += numero*nor_pre;      //Salva o contador normal.
     contador_P += numero*!(nor_pre);   //Salva o contador preferêncial.
 
   numero = (contador_N*nor_pre) + (contador_P*!(nor_pre); //nor_pre : em 1 mostra o normal, em 0 preferêncial.
+
+  Este pedaço de codigo talvez não seja necessario */
 
   mil = numero/1000;                  //Coloca o valor da casa do milhar em mil.
   cen = (numero%1000)/100;            //Coloca o valor da casa da centena em cen.
@@ -89,43 +92,72 @@ void Numero_Display (int numero){
 //=== Função Varrer o Teclado ===
 void LeTeclado(){
     int N_linha = 0;
+    int linha = 0;
+    int valor = 0;
     DDRD = 0xF0;                      //Os bits 0 à 3 entradas, os bits 4 à 7 saida.
     PORTD = '0b10010000';             //Coloca na primeira Linha do teclado.
     do{
         do{
             N_linha++;
-            if(PIND&col1){
-                Flag_botao1 = 0x01;}      //Testa o aperto do botao.
+            if(PIND&col4_Fx){ //Testa o aperto do botao.
+               Flag_botao4 = 0x01;
+               linha = N_linha;
+               }     
+            if(!(PIND&col4_Fx)&& Flag_botao4) {
+                Flag_botao4 = 0x00;
+                if(linha == 1){
+                  contador_N++;
+                  linha = 0;
+                }
+                else if(linha == 2){
+                  contador_N--;
+                  linha = 0;
+                }
+                else if(linha == 3){
+                  contador_P++;
+                  linha = 0;
+                }
+                else if(linha == 4){
+                  contador_P--;
+                  linha = 0;
+                }
+            }     
+            if(PIND&col1){//Testa o aperto do botao.
+              Flag_botao1 = 0x01;
+              linha = N_linha;  
+            }      
             if(!(PIND&col1)&& Flag_botao1) {
                 Flag_botao1 = 0x00;
-
+                if(linha == 1 && Flag_Start){
+                  valor = 1;
+                  linha = 0;
+                }
+                else if(linha == 2 && Flag_Start){
+                  valor = 4;
+                  linha = 0;
+                }
+                else if(linha == 3 &&Flag_Start){
+                  valor = 7;
+                  linha = 0;
+                }
+                else if(linha == 4 &&!(Flag_Start)){
+                  Flag_Start = 1;
+                  linha = 0;
+                }
             }
-
             if(PIND&col2){
                Flag_botao2 = 0x01;}      //Testa o aperto do botao.
-          if(!(PIND&col2)&& Flag_botao2) {
+            if(!(PIND&col2)&& Flag_botao2) {
                 Flag_botao2 = 0x00;
-
             }
-
             if(PIND&col3){
                Flag_botao3 = 0x01;}      //Testa o aperto do botao.
-          if(!(PIND&col3)&& Flag_botao3) {
+            if(!(PIND&col3)&& Flag_botao3) {
                 Flag_botao3 = 0x00;
 
             }
-
-            if(PIND&col4_Fx){
-               Flag_botao4 = 0x01;}      //Testa o aperto do botao.
-          if(!(PIND&col4_Fx)&& Flag_botao4) {
-                Flag_botao4 = 0x00;
-                if(N_linha == 1) 
-
-            }
-
-
-        }
-    }
+        }//end do-while
+    }//end do-while
 
 }
 
